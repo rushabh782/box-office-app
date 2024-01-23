ecee381cf2c2f694d2146c0b42d30123a6d7d224
rushabh782 <rushabhpatel877@gmail.com>
2024-01-23 12:06:48 +0530

    extract data fetching logic into custom hook useShowById

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,36 +1,40 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {getShowById} from '../api/tvmaze';
+import { getShowById } from '../api/tvmaze';
 
+const useShowById = showId => {
+  //custom  react hook
+  const [showData, setShowData] = useState(null);
+  const [showError, setShowError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (err) {
+        setShowError(err);
+      }
+    }
+    fetchData();
+  }, [showId]);
+
+  return { showData, showError };
+};
 
 const Show = () => {
-    const {showId} = useParams();
+  const { showId } = useParams();
 
-    const [showData,setShowData] = useState(null);
-    const [showError,setShowError] = useState(null);
+  const { showData, showError } = useShowById(showId);
 
-    useEffect( () => {
-        async function fetchData() {
-        try{
-            const data = await getShowById(showId);
-            setShowData(data);
-        }catch(err){
-            setShowError(err);
-        }
-          }
-          fetchData();
-        
-    },[showId]);
+  if (showError) {
+    return <div>We have an error: {showError.message}</div>;
+  }
+  if (showData) {
+    return <div>Got show data:{showData.name}</div>;
+  }
 
-    if(showError){
-        return <div>We have an error: {showError.message}</div>;
-    }
-    if(showData){
-        return <div>Got show data:{showData.name}</div>;
-    }
+  return <div>Data is loading</div>;
+};
 
-    return <div>Data is loading</div>;
-  };
-  
-  export default Show;
-  
+export default Show;
