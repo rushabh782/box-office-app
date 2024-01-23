d4de8f44a20beb5259629f3922ee973c7e092253
rushabh782 <rushabhpatel877@gmail.com>
2024-01-23 13:10:10 +0530

    installed react-query.replaced useShowById with useQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
 
+const queryClient = new QueryClient()
+
+
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
@@ -14,8 +19,6 @@ function App() {
         </Route>
 
         <Route path="/show/:showId" element={<Show />} />
-
-
 
         <Route path="*" element={<div>Not Found</div>} />
         {/* <Route path="/" element={<App />}>
@@ -33,6 +36,7 @@ function App() {
       <Route path="contact-us" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
